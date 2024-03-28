614d5194ee037903ec7061290acc1a4dcdceae29
Mikemupararano <kudath@yahoo.co.uk>
2024-03-28 11:40:29 +0000

    Added dependencies.

---
+++ react-charts/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="dataCard revenueCard">Chart 1</div>
+      <div className="dataCard customerCard">
+        <Bar
+          data={{
+            labels: ["A", "B", "C"],
+            datasets: [
+              {
+                label: "Revenue",
+                data: [200, 300, 400],
+              }
+            ]
+          }}
+        />
+      </div>
+      <div className="dataCard categoryCard">Chart 3</div>
+    </div>
+  );
+};
+
+export default App;
